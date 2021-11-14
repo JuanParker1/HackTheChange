94491b448a3042d57ad2637aa29a5b96a7a7a1cc
kameshwarasekar <kameshwarasekar@gmail.com>
2021-11-14 07:59:31 -0700

    recent changes

--- myapi/models.py
+++ myapi/models.py
@@ -19,7 +19,7 @@ class users(models.Model):
 
 class tasks(models.Model):
     task_id = models.AutoField(primary_key=True)
-    task_name = models.IntegerField()
+    task_name = models.CharField(max_length=20)
     task_description = models.IntegerField()
 
     class Meta:
